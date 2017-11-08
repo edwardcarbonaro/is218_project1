837c2c409ed0eb1677554696c1988ac4b3a76c83
EdwardCarbonaro <edward.carbonaro12@gmail.com>
2017-11-08 09:54:55 -0500

    Adds all database attributes to query

--- db_insert.php
+++ db_insert.php
@@ -2,8 +2,11 @@
 require "db.php";
 
 
-$sql = "INSERT INTO accounts(id,fname,email,lname)
-		VALUES(id,'" . $_POST["firstName"] . "','" . $_POST["lastName"] . "','" . $_POST["email"] . "')";
+$sql = "INSERT INTO accounts(id,fname,email,lname,phone,birthday,gender)
+		VALUES(id,'" . $_POST["firstName"] . "','" . $_POST["lastName"] . "','" . $_POST["email"] . "',
+				'" . $_POST["phoneNumber"] . "','" . $_POST["birthday"] . "','" . $_POST["gender"] . "')";
 runQuery($sql);
+
+echo $_POST["firstName"] . " " . $_POST["lastName"] . " has been registered";
 
 ?>
