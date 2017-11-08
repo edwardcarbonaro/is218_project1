d9de4132b9328d0b0da72f94dbecc29a18a5d715
EdwardCarbonaro <edward.carbonaro12@gmail.com>
2017-11-08 08:57:46 -0500

    Adds sql insert query

--- db_insert.php
+++ db_insert.php
@@ -1,7 +1,8 @@
 <?php
 require "db.php";
 
-
-
+$sql = "INSERT INTO accounts(fname,lname,email,phone,birthday,gender) 
+		VALUES(firstName,lastName,email,phoneNumber,birthday,gender)";
+runQuery($sql);
 
 ?>
