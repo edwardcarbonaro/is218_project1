3907dc1dc7198d8314a57d5ba503e44eee901f21
EdwardCarbonaro <edward.carbonaro12@gmail.com>
2017-11-08 20:23:16 -0500

    Checks to see if password matches the email

--- db_login.php
+++ db_login.php
@@ -4,20 +4,33 @@ $email = $_POST["email"];
 $password = $_POST["password"];
 
 $sql = "SELECT email FROM accounts WHERE email='$email'";
-if(runQuery($sql)==true)
-	{
-		$sql = "SELECT fname,lname FROM accounts where email='$email'";
+if(runQuery($sql)==true){	
+	$sql = "SELECT password FROM accounts WHERE email='$email'";
+	$pw = runQuery($sql);
+	
+	foreach($pw as $x){
+		$pw = $x["password"];
+	}
+
+	
+	if($password == $pw){
+		$sql = "SELECT fname,lname FROM accounts WHERE email='$email'";
 		$results = runQuery($sql);		
 		foreach($results as $row){
 			echo $row["fname"] . " " . $row["lname"];
 			
-		}
-
+		}			
 	}
-else
-{
+	else{
+		echo "Email was found but password does not match";
+	}
+
+			
+}	
+else{
 	echo "That email does not exist in the database";
 }
+
 
 
 ?>
